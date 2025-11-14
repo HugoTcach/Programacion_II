2456112cc4733e9eaaa3e4146be6f814a5b97e25
Hugo Tcach <hugotcach@gmail.com>
2025-11-14 09:52:49 -0300

    Docs(Integrante 1): Añade diagrama de clases UML

---
+++ OneDrive/Documentos/NetBeansProjects/TPI_Programacion2/src/main/java/utn/tfi/programacion2/main/AppMain.java
@@ -0,0 +1,8 @@
+package utn.tfi.programacion2.main;
+
+public class AppMain {
+    public static void main(String[] args) {
+        AppMenu app = new AppMenu();
+        app.run();
+    }
+}
---
+++ OneDrive/Documentos/NetBeansProjects/TPI_Programacion2/src/main/java/utn/tfi/programacion2/main/MenuDisplay.java
@@ -0,0 +1,24 @@
+package utn.tfi.programacion2.main;
+
+public class MenuDisplay {
+
+    public static void mostrarMenuPrincipal() {
+        System.out.println("\n========= MENU PRINCIPAL =========");
+        System.out.println("1. Gestionar Pedidos");
+        System.out.println("2. Gestionar Envios");
+        System.out.println("0. Salir");
+        System.out.print("Ingrese una opción: ");
+    }
+
+    public static void mostrarSubMenu(String titulo) {
+        System.out.println("\n===== " + titulo.toUpperCase() + " =====");
+        System.out.println("1. Crear");
+        System.out.println("2. Buscar por ID");
+        System.out.println("3. Listar");
+        System.out.println("4. Actualizar");
+        System.out.println("5. Eliminar (baja lógica)");
+        System.out.println("6. Buscar por campo único");
+        System.out.println("0. Volver");
+        System.out.print("Ingrese una opción: ");
+    }
+}
